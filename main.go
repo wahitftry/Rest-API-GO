db560b2e5e48202a500fcf95eed230104e6270ae
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-06-20 11:17:34 +0700

    update lagi

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+    "errors"
     "net/http"
+    "sort"
+    "strconv"
+    "strings"
 
     "github.com/labstack/echo/v4"
 )
@@ -26,13 +30,55 @@ var foodMenu = []MenuItem{
 }
 
 func getFoodMenu(c echo.Context) error {
-    return c.JSON(http.StatusOK, foodMenu)
+    q := c.QueryParam("q")
+    sortBy := c.QueryParam("sort_by")
+    order := c.QueryParam("order")
+    limit, err := strconv.Atoi(c.QueryParam("limit"))
+    if err != nil {
+        limit = len(foodMenu)
+    }
+    filteredMenu := foodMenu
+    if q != "" {
+        filteredMenu = make([]MenuItem, 0)
+        for _, item := range foodMenu {
+            if strings.Contains(strings.ToLower(item.Name), strings.ToLower(q)) ||
+                strings.Contains(strings.ToLower(item.OrderCode), strings.ToLower(q)) {
+                filteredMenu = append(filteredMenu, item)
+            }
+        }
+    }
+    if sortBy != "" {
+        sort.Slice(filteredMenu, func(i, j int) bool {
+            switch sortBy {
+            case "name":
+                if order == "desc" {
+                    return filteredMenu[i].Name > filteredMenu[j].Name
+                }
+                return filteredMenu[i].Name < filteredMenu[j].Name
+            case "price":
+                if order == "desc" {
+                    return filteredMenu[i].Price > filteredMenu[j].Price
+                }
+                return filteredMenu[i].Price < filteredMenu[j].Price
+            default:
+                return i < j
+            }
+        })
+    }
+    if len(filteredMenu) > limit {
+        filteredMenu = filteredMenu[:limit]
+    }
+
+    return c.JSON(http.StatusOK, filteredMenu)
 }
 
 func addFoodMenu(c echo.Context) error {
     item := new(MenuItem)
     if err := c.Bind(item); err != nil {
         return err
+    }
+    if item.Name == "" || item.OrderCode == "" {
+        return errors.New("Invalid input")
     }
     foodMenu = append(foodMenu, *item)
     return c.JSON(http.StatusOK, item)
@@ -57,6 +103,9 @@ func updateFoodMenu(c echo.Context) error {
             if err := c.Bind(newItem); err != nil {
                 return err
             }
+            if newItem.Name == "" || newItem.OrderCode == "" {
+                return errors.New("Invalid input")
+            }
             foodMenu[i] = *newItem
             return c.JSON(http.StatusOK, newItem)
         }
@@ -66,9 +115,11 @@ func updateFoodMenu(c echo.Context) error {
 
 func main() {
     e := echo.New()
-    e.GET("/menu/food", getFoodMenu)
-    e.POST("/menu/food", addFoodMenu)
-    e.DELETE("/menu/food/:orderCode", removeFoodMenu)
-    e.PUT("/menu/food/:orderCode", updateFoodMenu)
-    e.Logger.Fatal(e.Start(":2000"))
+
+    e.GET("/menu", getFoodMenu)
+    e.POST("/menu", addFoodMenu)
+    e.DELETE("/menu/:orderCode", removeFoodMenu)
+    e.PUT("/menu/:orderCode", updateFoodMenu)
+
+    e.Logger.Fatal(e.Start(":8080"))
 }
