3f191598ede5044fc16a956acb517908ed9d3cfd
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-06-22 00:34:01 +0700

    update

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ type MenuItem struct {
 	Price     int    `json:"price"`
 }
 
-var (
+const (
 	DefaultMenuLimit = 100
-	SortByNames = "name"
-	SortByPrice = "price"
-	Ascending = "asc"
-	Descending = "desc"
+	SortByNames      = "name"
+	SortByPrice      = "price"
+	Ascending        = "asc"
+	Descending       = "desc"
 )
 
 var foodMenu = []MenuItem{
@@ -37,41 +37,96 @@ var foodMenu = []MenuItem{
 	},
 }
 
+func validateMenuItem(item *MenuItem) error {
+	if item.Name == "" || item.OrderCode == "" {
+		return errors.New("name and order code are required")
+	}
+	return nil
+}
+
+func validateLimit(limitStr string) (int, error) {
+	if limitStr == "" {
+		return DefaultMenuLimit, nil
+	}
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limit < 0 {
+		return 0, errors.New("limit must be a positive integer")
+	}
+	return limit, nil
+}
+
+func filterMenu(menu []MenuItem, query string) []MenuItem {
+	if query == "" {
+		return menu
+	}
+	filteredMenu := make([]MenuItem, 0)
+	for _, item := range menu {
+		if strings.Contains(strings.ToLower(item.Name), strings.ToLower(query)) ||
+			strings.Contains(strings.ToLower(item.OrderCode), strings.ToLower(query)) {
+			filteredMenu = append(filteredMenu, item)
+		}
+	}
+	return filteredMenu
+}
+
+func (m MenuItem) lessThan(other MenuItem, sortBy string, order string) bool {
+	switch sortBy {
+	case SortByNames:
+		if order == Descending {
+			return m.Name > other.Name
+		}
+		return m.Name < other.Name
+	case SortByPrice:
+		if order == Descending {
+			return m.Price > other.Price
+		}
+		return m.Price < other.Price
+	default:
+		return false
+	}
+}
+
+type By func(m1, m2 *MenuItem) bool
+
+func (by By) Sort(menu []MenuItem) {
+	ms := &menuItemSorter{
+		menu: menu,
+		by:   by,
+	}
+	sort.Sort(ms)
+}
+
+type menuItemSorter struct {
+	menu []MenuItem
+	by   func(m1, m2 *MenuItem) bool
+}
+
+func (s *menuItemSorter) Len() int {
+	return len(s.menu)
+}
+
+func (s *menuItemSorter) Swap(i, j int) {
+	s.menu[i], s.menu[j] = s.menu[j], s.menu[i]
+}
+
+func (s *menuItemSorter) Less(i, j int) bool {
+	return s.by(&s.menu[i], &s.menu[j])
+}
+
 func getFoodMenu(c echo.Context) error {
 	q := c.QueryParam("q")
 	sortBy := c.QueryParam("sort_by")
 	order := c.QueryParam("order")
-	limit, err := strconv.Atoi(c.QueryParam("limit"))
-	if err != nil || limit < 0 {
-		limit = DefaultMenuLimit
+	limitStr := c.QueryParam("limit")
+	limit, err := validateLimit(limitStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	filteredMenu := foodMenu
-	if q != "" {
-		filteredMenu = make([]MenuItem, 0)
-		for _, item := range foodMenu {
-			if strings.Contains(strings.ToLower(item.Name), strings.ToLower(q)) ||
-				strings.Contains(strings.ToLower(item.OrderCode), strings.ToLower(q)) {
-				filteredMenu = append(filteredMenu, item)
-			}
-		}
-	}
+	filteredMenu := filterMenu(foodMenu, q)
 	if sortBy != "" {
-		sort.Slice(filteredMenu, func(i, j int) bool {
-			switch sortBy {
-			case SortByNames:
-				if order == Descending {
-					return filteredMenu[i].Name > filteredMenu[j].Name
-				}
-				return filteredMenu[i].Name < filteredMenu[j].Name
-			case SortByPrice:
-				if order == Descending {
-					return filteredMenu[i].Price > filteredMenu[j].Price
-				}
-				return filteredMenu[i].Price < filteredMenu[j].Price
-			default:
-				return i < j
-			}
-		})
+		By(func(m1, m2 *MenuItem) bool {
+			return m1.lessThan(*m2, sortBy, order)
+		}).Sort(filteredMenu)
 	}
 	if len(filteredMenu) > limit {
 		filteredMenu = filteredMenu[:limit]
@@ -82,10 +137,10 @@ func getFoodMenu(c echo.Context) error {
 func addFoodMenu(c echo.Context) error {
 	item := new(MenuItem)
 	if err := c.Bind(item); err != nil {
-		return c.JSON(http.StatusBadRequest, "Invalid input")
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	if item.Name == "" || item.OrderCode == "" {
-		return c.JSON(http.StatusBadRequest, "Invalid input")
+	if err := validateMenuItem(item); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 	foodMenu = append(foodMenu, *item)
 	return c.JSON(http.StatusOK, item)
@@ -104,14 +159,14 @@ func removeFoodMenu(c echo.Context) error {
 
 func updateFoodMenu(c echo.Context) error {
 	code := c.Param("orderCode")
-	for i, item := range foodMenu {
-		if item.OrderCode == code {
+	for i := range foodMenu {
+		if foodMenu[i].OrderCode == code {
 			newItem := new(MenuItem)
 			if err := c.Bind(newItem); err != nil {
-				return c.JSON(http.StatusBadRequest, "Invalid input")
+				return c.JSON(http.StatusBadRequest, err.Error())
 			}
-			if newItem.Name == "" || newItem.OrderCode == "" {
-				return c.JSON(http.StatusBadRequest, "Invalid input")
+			if err := validateMenuItem(newItem); err != nil {
+				return c.JSON(http.StatusBadRequest, err.Error())
 			}
 			foodMenu[i] = *newItem
 			return c.JSON(http.StatusOK, newItem)
